0ff280fcf8229448a8ab50f0a56fe80b05717fab
TCblack555 <76724852+TCblack555@users.noreply.github.com>
2021-11-02 20:23:50 +0800

    pull commit
    modify remote

--- src/main/java/com/gec/git/GitTest.java
+++ src/main/java/com/gec/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("lalalal!");
+        System.out.println("modify remote!");
     }
 }
