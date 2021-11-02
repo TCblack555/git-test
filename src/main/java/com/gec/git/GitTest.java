e671b79c43ffd78f9ca909cc4ab7d519f92f09e5
TCblack555 <76724852+TCblack555@users.noreply.github.com>
2021-11-02 21:38:02 +0800

    !!!

--- src/main/java/com/gec/git/GitTest.java
+++ src/main/java/com/gec/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("lalalal!");
         System.out.println("modify remote!");
+        System.out.println("gee gee gee gee baby baby baby!");
     }
 }
